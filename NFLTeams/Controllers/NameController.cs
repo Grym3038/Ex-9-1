e58f414e8de941046738e10eee84652e6a12f3c8
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-02-07 15:11:27 -0600

    added files

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers {
+    public class NameController : Controller {
+
+        [HttpGet]
+        public IActionResult Index() 
+        {
+            var session = new NFLSession(HttpContext.Session);
+
+            var model = new TeamListViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams(),
+                UserName = session.GetUserName()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Change(TeamViewModel model) 
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.SetUserName("");
+            session.SetUserName(model.UserName);
+
+            return RedirectToAction("Index", "Home", new {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv()
+            });
+        }
+    }
+}
---
+++ NFLTeams/Models/TeamViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NFLTeams.Models {
+    public class TeamViewModel {
+        public Team Team { get; set; }
+        public string ActiveConf { get; set; }
+        public string ActiveDiv { get; set; }
+        public string UserName { get; set; }
+    }
+}
